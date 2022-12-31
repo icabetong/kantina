20557927a05fad3b93c35797b3dc0e8c436cee14
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2022-12-31 21:56:28 +0800

    Added Initial About Page

--- src/routes/(app)/(static)/about/+page.svelte
+++ src/routes/(app)/(static)/about/+page.svelte
@@ -1,15 +1,34 @@
-<script lang="ts">
-	import EmptyState from '$components/empty-state/EmptyState.svelte'
-</script>
-
-<div class="page min-h-screen">
-	<h1 class="page-header">About</h1>
-	<div class="h-full">
-		<EmptyState
-			icon="/images/under-construction.svg"
-			title="Under Construction"
-			message="This page have not been built yet, check it out later." />
-	</div>
+<div class="min-h-screen page">
+	<section
+		class="px-8 pt-8 pb-32 flex flex-col items-center justify-center text-center md:px-24 md:pt-16">
+		<h1 class="uppercase text-orange-600 text-sm font-semibold mb-4">Our Purpose</h1>
+		<h2 class="font-black text-3xl md:text-4xl">Helping small businesses digitalize</h2>
+		<p class="text-center text-gray-600 mt-6">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias facilis quibusdam
+			necessitatibus sit animi, esse id. Ea sint, a provident velit quo natus quisquam tenetur
+			corrupti, dolore quaerat incidunt asperiores.
+		</p>
+	</section>
+	<section
+		class="flex flex-col-reverse items-center justify-center px-8 py-0 md:px-10 md:py-16 gap-16 md:flex-row">
+		<div class="w-full md:w-1/2">
+			<div class="uppercase text-orange-600 text-sm font-semibold mb-4">Our Mission</div>
+			<h3 class="font-black text-3xl mb-4">Who we are</h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laborum exercitationem
+				dolore fuga facilis quod quibusdam ad modi officiis? Mollitia magni porro, perferendis
+				distinctio a ducimus. Maiores deserunt explicabo optio.
+			</p>
+		</div>
+		<div class="w-full md:w-1/2">
+			<div class="bg-gradient-to-br from-orange-500 to-pink-500 pb-2 rounded-lg drop-shadow-2xl">
+				<img
+					src="https://images.unsplash.com/photo-1589553288887-3ee1f709e119?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1596&q=80"
+					alt="small street stall owner"
+					class="rounded-md shadow" />
+			</div>
+		</div>
+	</section>
 </div>
 
 <svelte:head>
