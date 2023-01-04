26abd8f207453d2b9e13d3742fff08205e709052
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2023-01-04 19:06:18 +0800

    Added unit tests

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,6 +1,22 @@
 import pocketbase from './backend'
 
-export function parseFileUrl(collection: string, id: string, fileName: string, size?: string) {
+/**
+ * @description
+ * This concatinates the information required to build the URL used
+ * to fetch the file stored in the backend.
+ *
+ * @param collection The collection or table name where the image is referenced
+ * @param id The id of the row where the image is referenced
+ * @param fileName The filename of the file
+ * @param size Optional size options to specify the thumbnail size
+ * @returns the URL of the file where it can be used to download
+ */
+export function parseFileUrl(
+	collection: string,
+	id: string,
+	fileName: string,
+	size?: string
+): string {
 	// example url:
 	// http://127.0.0.1:8090/api/files/example/kfzjt5oy8r34hvn/test_52iWbGinWd.png
 
